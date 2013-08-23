746e9419ca104d113de6e56e0da1989fa68d9a50
Noah Wood <noahwood@gmail.com>
2013-08-22 21:19:40 -0400

    Initialize repository

---
+++ TestDB/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestDB::Application.config.secret_key_base = 'b3a743efdf5b5bb9301d71355e786fa90df768629469a4d51bd9a89638644fb389bfff8aa41b6bdae214374abb1036a2242615ed7e314e9e628f3f6a454be4ee'
